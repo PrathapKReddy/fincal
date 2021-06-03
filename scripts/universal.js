846f647c075eabd63b3bd7217275b92679bc3658
kprathapreddy1997 <76874071+kprathapreddy1997@users.noreply.github.com>
2021-06-03 21:02:07 +0530

    Add files via upload

--- scripts/universal.js
+++ scripts/universal.js
@@ -169,4 +169,15 @@ async function registerSW() {
   }
 }
 
-
+// Code for Web Share API
+function shareMe(){
+   if (navigator.share) {
+     navigator.share({
+       title: 'web.dev',
+       text: 'Check out web.dev.',
+       url: 'https://web.dev/',
+     })
+       .then(() => console.log('Successful share'))
+       .catch((error) => console.log('Error sharing', error));
+   }
+}
